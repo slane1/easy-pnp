b96f33b4372a4c6b3bbaaeb3f7cc53c8ce7d46ec
Stefan Lange <stefanlange@maeve.de>
2024-05-09 14:38:46 +0200

    init backend

---
+++ backend/src/test/java/de/slar/easypnp/backend/EasyPnpApplicationTests.java
@@ -0,0 +1,13 @@
+package de.slar.easypnp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
